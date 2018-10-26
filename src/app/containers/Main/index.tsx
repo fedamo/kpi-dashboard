56db9e0b8afd375d8076fab1279d5bd119a05f88
Waylon <fedamo@gmail.com>
2018-10-26 16:04:55 -0400

    finalised

--- src/app/containers/Main/index.tsx
+++ src/app/containers/Main/index.tsx
@@ -1,43 +1,162 @@
 import * as React from "react";
-import { Grid, Responsive, Tab, Container } from "semantic-ui-react";
+import { Grid, Responsive, Tab, Container, Table } from "semantic-ui-react";
 
 import Plot from "react-plotly.js";
-import data1 from "./data1.json";
+import data_conv_rate from "./data_conv_rate.json";
+import data_conversions from "./data_conversions.json";
+import data_cost from "./data_cost.json";
+import data_impressions from "./data_impressions.json";
+import data_clicks from "./data_clicks.json";
+import data_cost_conv from "./data_cost_conv.json";
+
+import data_sales from "./data_sales.json";
+import data_units from "./data_units.json";
+import data_orders from "./data_orders.json";
+
+
+var selectorOptions = {
+  buttons: [
+    {
+      step: "month",
+      stepmode: "backward",
+      count: 1,
+      label: "1m"
+    },
+    {
+      step: "month",
+      stepmode: "backward",
+      count: 6,
+      label: "6m"
+    },
+    {
+      step: "year",
+      stepmode: "todate",
+      count: 1,
+      label: "YTD"
+    },
+    {
+      step: "year",
+      stepmode: "backward",
+      count: 1,
+      label: "1y"
+    },
+    {
+      step: "all"
+    }
+  ]
+};
+
+
+var updatemenus = [
+{active: 0,
+ 
+  buttons: [
+        {
+      "args": ["visible", [true, false,"legendonly", false,"legendonly", false,"legendonly", false,"legendonly", false,"legendonly", false,"legendonly", false]], 
+      "label": "Daily", 
+      "method": "restyle"
+    }, 
+    {
+      "args": ["visible", [false, true,false, "legendonly",false, "legendonly",false, "legendonly",false, "legendonly",false, "legendonly",false, "legendonly"]], 
+      "label": "Monthly", 
+      "method": "restyle"
+    }
+    ],
+    direction: "down", 
+    
+  yanchor: "top",
+    x: 0.55,
+    y: 1.05,
+    bgcolor: '#FFFAFA',
+    showactive: true,
+    visible: true
+}]
+
+
 
 const TabMapper = [
   {
-    title: "Conversion Rate",
-    data: data1
+    title: "Sales: Revenue ($)",
+    data: data_sales,
+    x: 'Date',
+    y: 'Dollars ($)'
+    
   },
   {
-    title: "Cost/Conversion",
-    data: data1
+    title: "Sales: Units",
+    data: data_units,
+    x:'Date',
+    y: ' Units'
   },
+ 
+  {
+    title: "Sales: Orders",
+    data: data_orders,
+    x:'Date',
+    y: 'Orders'
+  },
+  
   {
     title: "Clicks",
-    data: data1
+    data: data_clicks,
+    x:'Date',
+    y: 'Clicks'
   },
+ 
   {
     title: "Impressions",
-    data: data1
+    data: data_impressions,
+    x:'Date',
+    y: 'Clicks'
+  },
+
+  {
+    title: "Conversions",
+    data: data_conversions,
+    x:'Date',
+    y: 'Clicks'
+  },
+  {
+    title: "Conversion Rate (%)",
+    data: data_conv_rate,
+    x:'Date',
+    y: 'Clicks'
   },
   {
     title: "Cost",
-    data: data1
+    data: data_cost,
+    x:'Date',
+    y: 'Clicks'
   },
   {
-    title: "Conversions",
-    data: data1
-  },
-  {
-    title: "Sales Unit",
-    data: data1
-  },
-  {
-    title: "Sales Revenue",
-    data: data1
+    title: "Cost per Conversions",
+    data: data_cost_conv,
+    x:'Date',
+    y: 'Clicks'
   }
+
+
+  
 ];
+
+
+var legend_annotations = [
+  {
+      x:1.067,
+      y:1.035,
+      align:"right",
+      valign:"top",
+      text:'Affiliate Code Groups',
+      showarrow:false,
+      xref:"paper",
+      yref:"paper",
+      xanchor:"center",
+      yanchor:"top",
+      font: {"size":14}
+  }  ];
+
+
+
 
 const panes = TabMapper.map(dataset => {
   return {
@@ -47,13 +166,71 @@ const panes = TabMapper.map(dataset => {
         <Plot
           style={{ width: "100%", height: "100vh" }}
           data={dataset.data}
-          layout={{ autosize: true }}
+          layout={{
+            title: dataset.title,
+            annotations: legend_annotations,
+            legend:{bordercolor:'#b2b2b2'},
+            titlefont: {size: 26, color: '#7f7f7f'},
+            updatemenus: {active: -1,
+ 
+              buttons: [
+                    {
+                  "args": ["visible", [true, false,"legendonly", false,"legendonly", false,"legendonly", false,"legendonly", false,"legendonly", false,"legendonly", false]], 
+                  "label": "Daily", 
+                  "method": "restyle"
+                }, 
+                {
+                  "args": ["visible", [false, true,false, "legendonly",false, "legendonly",false, "legendonly",false, "legendonly",false, "legendonly",false, "legendonly"]], 
+                  "label": "Monthly", 
+                  "method": "restyle"
+                }
+                ],
+                direction: "down", 
+                
+              yanchor: "top",
+                x: 0.55,
+                y: 1.05,
+                bgcolor: '#FFFAFA',
+                showactive: true,
+                visible: true
+            },
+            xaxis: { rangeselector: selectorOptions,
+                      rangeslider: {},
+            
+            
+              //title: dataset.x|| "X Axis Default",
+              titlefont: {
+                family: "Courier New, monospace",
+                size: 18,
+                color: "#7f7f7f"
+              }
+            },
+          
+            yaxis: {
+            
+              titlefont: {
+                family: "Courier New, monospace",
+                size: 18,
+                color: "#7f7f7f"
+              },
+            rangeslider: {
+                visible : true},
+            },
+          
+            
+            }
+        }
+      
           useResizeHandler={true}
         />
       </Tab.Pane>
+      
     )
   };
 });
+
+
+
 
 const TestTabs = () => <Tab panes={panes} />;
 export class Main extends React.Component {
