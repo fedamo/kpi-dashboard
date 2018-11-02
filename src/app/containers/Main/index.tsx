46d8f86575fbdef8408637b148bf9f600612ad72
fedamo <fedamo@gmail.com>
2018-11-02 16:49:38 -0400

    git checkout -b branchName

--- src/app/containers/Main/index.tsx
+++ src/app/containers/Main/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { Grid, Responsive, Tab, Container, Table } from "semantic-ui-react";
+import { Component } from 'react';
+import { Grid, Responsive, Tab, Container, Table, Icon, Menu, Header, Divider, Image, Dropdown, Sidebar, Segment, DropdownItem} from "semantic-ui-react";
 import Plot from "react-plotly.js";
 import data_conv_rate from "./data_conv_rate.json";
 import data_conversions from "./data_conversions.json";
@@ -10,38 +11,37 @@ import data_cost_conv from "./data_cost_conv.json";
 import data_sales from "./data_sales.json";
 import data_units from "./data_units.json";
 import data_orders from "./data_orders.json";
+import data_ntf from "./data_ntf.json";
+import { select } from "d3";
+import mylogo from "./nfpa logo.png"
+import data_bar from "./data_bar.json"
+
 
 
 var selectorOptions = {
-  buttons: [
-    {
-      step: "month",
-      stepmode: "backward",
+  buttons: [{
+      step: 'month',
+      stepmode: 'backward',
       count: 1,
-      label: "1m"
-    },
-    {
-      step: "month",
-      stepmode: "backward",
+      label: '1m'
+  }, {
+      step: 'month',
+      stepmode: 'backward',
       count: 6,
-      label: "6m"
-    },
-    {
-      step: "year",
-      stepmode: "todate",
+      label: '6m'
+  }, {
+      step: 'year',
+      stepmode: 'todate',
       count: 1,
-      label: "YTD"
-    },
-    {
-      step: "year",
-      stepmode: "backward",
+      label: 'YTD'
+  }, {
+      step: 'year',
+      stepmode: 'backward',
       count: 1,
-      label: "1y"
-    },
-    {
-      step: "all"
-    }
-  ]
+      label: '1y'
+  }, {
+      step: 'all',
+  }],
 };
 
 
@@ -63,9 +63,10 @@ var updatemenus = [
     direction: "down", 
     
   yanchor: "top",
-    x: 0.57,
-    y: 1.05,
+    x: 0.056,
+    y: 1.130,
     bgcolor: '#FFFAFA',
+    font: {size:12, align: 'left'},
     showactive: true,
     visible: true
 }]
@@ -131,8 +132,13 @@ const TabMapper = [
     data: data_cost_conv,
     x:'Date',
     y: 'Clicks'
+  },
+  {
+    title: "New to File",
+    data: data_ntf,
+    x:'Date',
+    y: 'New to File'
   }
-
 
   
 ];
@@ -140,7 +146,7 @@ const TabMapper = [
 
 var legend_annotations = [
   {
-      x:1.067,
+      x:1.007,
       y:1.035,
       align:"right",
       valign:"top",
@@ -150,30 +156,76 @@ var legend_annotations = [
       yref:"paper",
       xanchor:"center",
       yanchor:"top",
-      font: {"size":14}
+      font: {"size":12}
   }  ];
 
+const TabMapper2 = [ 
+    {
+      title: "Product Groups",
+      data: data_bar,
+      x:'Sales ($)',
+      y: 'Product Groups'
+    }
 
+  ];
+  
+  
+  var legend_annotations = [
+    {
+        x:1.007,
+        y:1.035,
+        align:"right",
+        valign:"top",
+        text:'Affiliate Code Groups',
+        showarrow:false,
+        xref:"paper",
+        yref:"paper",
+        xanchor:"center",
+        yanchor:"top",
+        font: {"size":12}
+    }  ];
+  
 
 
 const panes = TabMapper.map(dataset => {
   return {
-    menuItem: dataset.title,
+    menuItem: {icon: 'chart line', name: dataset.title},
     render: () => (
       <Tab.Pane>
         <Plot
-          style={{ width: "100%", height: "100vh" }}
+          style={{ width: "100%", height: "90vh" }}
           data={dataset.data}
           layout={{autosize: true,
             title: dataset.title,
-            annotations: legend_annotations,
             legend:{bordercolor:'#b2b2b2'},
             titlefont: {size: 26, color: '#7f7f7f'},
             updatemenus: updatemenus,
-            xaxis: { rangeselector: selectorOptions,
-                      rangeslider: {},
-            
-            
+            xaxis: { 
+              rangeselector: {buttons: [{
+                step: 'month',
+                stepmode: 'backward',
+                count: 1,
+                label: '1m'
+            }, {
+                step: 'month',
+                stepmode: 'backward',
+                count: 6,
+                label: '6m'
+            }, {
+                step: 'year',
+                stepmode: 'todate',
+                count: 1,
+                label: 'YTD'
+            }, {
+                step: 'year',
+                stepmode: 'backward',
+                count: 1,
+                label: '1y'
+            }, {
+                step: 'all',
+            }]},
+              rangeslider: {
+                visible : true},
               //title: dataset.x|| "X Axis Default",
               titlefont: {
                 family: "Courier New, monospace",
@@ -189,8 +241,7 @@ const panes = TabMapper.map(dataset => {
                 size: 18,
                 color: "#7f7f7f"
               },
-            rangeslider: {
-                visible : true},
+         
             },
           
             
@@ -205,21 +256,76 @@ const panes = TabMapper.map(dataset => {
   };
 });
 
+const panes1 = TabMapper2.map(dataset => {
+  return {
+    menuItem: {icon: 'chart line', name: dataset.title},
+    render: () => (
+      <Tab.Pane >
+        <Plot
+          style={{ width: "100%", height: "90vh" }}
+          data={dataset.data}
+          layout={{autosize: true,
+            title: dataset.title,
+            legend:{bordercolor:'#b2b2b2'},
+          barmode: 'group'}}
+      
+          useResizeHandler={true}
+        />
+      </Tab.Pane>
+      
+    )
+  };
+});
 
 
+const TestTabs = () => <Tab renderActiveOnly={true} menu={{  secondary: true, attached: true, tabular:true,pointing: true }} menuPosition='left' panes={panes}  />;
+const TestTabs1 = () => <Tab renderActiveOnly={true} menu={{  secondary: true, attached: true, tabular:true,pointing: true }} menuPosition='left' panes={panes1}  />;
+const TestTabs2 = () => <Tab renderActiveOnly={true} menu={{  tabular:true,pointing: true }} menuPosition='left' panes={panes1}  />;
 
-const TestTabs = () => <Tab panes={panes} />;
+
+const SelectedTab = ({index}) => {
+  switch(index){
+     case 0:
+      return <TestTabs/>
+    case 1: 
+      return <TestTabs1/>
+    case 2: 
+      return <TestTabs2/>
+  }
+}
+const options = [
+  { key: 1, text: 'KPI Trends', value: 1 },
+  { key: 2, text: 'Product Attribution', value: 2 },
+  { key: 3, text: 'Customer Insight', value: 3 },
+]
+
+
 export class Main extends React.Component {
+
+  // add component state
+  state = {
+    selectedIndex: 0
+  }
+
   componentDidMount() {}
   render() {
     return (
-      <Grid verticalAlign="top">
-        <Grid.Row columns={1} align="center">
-          <Grid.Column align="top">
-            <TestTabs />
-          </Grid.Column>
-        </Grid.Row>
+      <Grid columns='equal'>
+        <Grid.Column>
+
+          <Dropdown  placeholder='Select Metrics'  fluid selection   options = {options}  onClick={()=>this.setState({selectedIndex: options.value })}  >
+            {/* <Dropdown.Menu   > */}
+            {/* <Dropdown.Header icon='tags' content='Select Metrics' /> */}
+              {/* <Dropdown.Item text = 'KPI Trends'  onClick={()=>this.setState({selectedIndex: 0})}></Dropdown.Item> */}
+              {/* <Dropdown.Item text = 'Product Attribution' onClick={()=>this.setState({selectedIndex: 1})}></Dropdown.Item> */}
+              {/* <Dropdown.Item text = 'Customer Insight' onClick={()=>this.setState({selectedIndex: 2})}></Dropdown.Item> */}
+            {/* </Dropdown.Menu> */}
+          </Dropdown>
+
+          <SelectedTab index={this.state.selectedIndex}/>
+        </Grid.Column>
       </Grid>
     );
   }
 }
+
